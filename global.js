a7f03ee82b2aa7642a386a581401732e93321f6b
INQO <mszyszka.web@gmail.com>
2024-05-26 20:40:05 +0200

    add new file global.js

---
+++ global.js
@@ -0,0 +1,10 @@
+// Global object
+// console.log(global);
+
+setTimeout(() => {
+    console.log('in the timeout');
+}, 3000)
+
+const int = setInterval(() => {
+    console.log('Siema Mati');
+}, 1000)
--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-const name = 'mario';
+const name = 'Inqo';
 console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('Mario');
+greet('Inqo');
