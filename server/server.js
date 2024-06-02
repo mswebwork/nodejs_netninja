d8ceec8be4801c545896fd82e85daa4e87200e84
INQO <mszyszka.web@gmail.com>
2024-06-02 02:13:53 +0200

    information about response object

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  console.log('Req OBJ:' , req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>')
+  res.write('<h1>hello, inqo</h1>');
+  res.write('<p>hello, again inqo</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
