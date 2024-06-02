25e0c4e9aa448580b559b53a292d9fe43da8a99d
INQO <mszyszka.web@gmail.com>
2024-06-02 12:18:25 +0200

    add routing

--- server/server.js
+++ server/server.js
@@ -7,13 +7,26 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
+
   // res.write('<head><link rel="stylesheet" href="#"></head>')
   // res.write('<h1>hello, inqo</h1>');
   // res.write('<p>hello, again inqo</p>');
   // res.end();
 
   // send HTML file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
