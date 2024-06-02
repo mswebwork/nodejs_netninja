2b524964acf2038effef08b87b2f548b700f342c
INQO <mszyszka.web@gmail.com>
2024-06-02 02:22:41 +0200

    return html file in browser

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log('Req OBJ:' , req.url, req.method);
@@ -6,10 +7,23 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
-  res.write('<head><link rel="stylesheet" href="#"></head>')
-  res.write('<h1>hello, inqo</h1>');
-  res.write('<p>hello, again inqo</p>');
-  res.end();
+  // res.write('<head><link rel="stylesheet" href="#"></head>')
+  // res.write('<h1>hello, inqo</h1>');
+  // res.write('<p>hello, again inqo</p>');
+  // res.end();
+
+  // send HTML file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write();
+      res.end(data);
+    }
+
+  });
+
 });
 
 server.listen(3000, 'localhost', () => {
